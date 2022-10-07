81fe691adca90880a723f01f0e2962ec261e3132
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2022-10-07 22:56:06 +0900

    おまけの実装もおわり

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ function Square(props){
       <button
         className="square"
         onClick={props.onClick}
+        style={{background: props.highlight ? "#ffffaa" : "#ffffff"}}
       >
         {props.value}
       </button>
@@ -21,8 +22,10 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        key={i}
         value={this.props.squares[i]}
         onClick={() => {this.props.onClick(i);}}
+        highlight={this.props.winner.line.indexOf(i) >= 0}
       />
     );
   }
@@ -31,27 +34,18 @@ class Board extends React.Component {
 
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {Array(3).fill(null).map((a, j) => (
+          <div className="board-row" key={j}>
+            {Array(3).fill(null).map((a, i) => (this.renderSquare(i + j * 3)))}
+          </div>
+        ))}
       </div>
     );
   }
 }
 
-function calculateWinner(squares) {
+function calculateWinner(board) {
+  const squares = board.squares;
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -65,17 +59,22 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {winner: squares[a], line: [a, b, c]};
     }
   }
-  return null;
+  return {winner: null, line: [-1, -1, -1]};
 }
 
 class Game extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      history: [Array(9).fill(null)],
+      history: [{
+        squares: Array(9).fill(null),
+        target: -1,
+        step: 0,
+      }],
+      historySortReverse: false,
       stepNum: 0,
     }
   }
@@ -90,35 +89,50 @@ class Game extends React.Component {
   }
   handleClick(i){
     let stepNum = this.state.stepNum;
-    let squares = this.latestBoard();
-    if(calculateWinner(squares) || squares[i]){
+    let squares = this.latestBoard().squares;
+    if(calculateWinner(this.latestBoard()).winner || squares[i]){
       return;
     }
     squares = squares.map((s, j) => (i == j ? (this.xIsNext() ? 'X' : 'O') : s));
     this.setState({
-      history: this.state.history.slice(0, stepNum + 1).concat([squares]),
+      history: this.state.history.slice(0, stepNum + 1).concat([{
+        squares: squares,
+        target: i,
+        step: stepNum + 1,
+      }]),
       stepNum: stepNum + 1,
     });
   }
   render() {
     const winner = calculateWinner(this.latestBoard());
     let status;
-    if(winner){
-      status = "Winner: " + winner;
+    if(winner.winner){
+      status = "Winner: " + winner.winner;
+    }else if(this.latestBoard().squares.indexOf(null) === -1){
+      status = "Draw";
     }else{
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      status = 'Next player: ' + (this.xIsNext() ? 'X' : 'O');
     }
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={this.latestBoard()} onClick={(i) => {this.handleClick(i);}}/>
+          <Board winner={winner} squares={this.latestBoard().squares} onClick={(i) => {this.handleClick(i);}}/>
         </div>
         <div className="game-info">
           <div>{status}</div>
+          <a href="#" onClick={() => {this.setState({historySortReverse: !this.state.historySortReverse})}}>
+            履歴を逆順に表示する
+          </a>
           <ol>{
-            this.state.history.map((sq, i) => (
+            (this.state.historySortReverse ? this.state.history.slice().reverse() : this.state.history).map((b, i) => (
               <li key={i}>
-                <button onClick={() => {this.jumpTo(i)}}>{i ? `Go to move #${i}` : "Go to game start"}</button>
+                <button style={{fontWeight: b.step === this.state.stepNum ? "bold" : "normal"}} onClick={() => {this.jumpTo(b.step)}}>
+                  {b.step ?
+                    `Go to move #${b.step} (${b.target % 3 + 1}, ${Math.floor(b.target / 3) + 1})`
+                  :
+                    "Go to game start"
+                  }
+                </button>
               </li>
             ))
           }</ol>
